31d29d740c1fa4dc7726b2dfe7caa2a1f3c87d9f
xujiangzhe <xujiangzhe@kuaishou.com>
2020-12-10 15:29:34 +0800

    123

---
+++ src/main/java/com/cskaoyan/mall/Dev.java
@@ -0,0 +1,11 @@
+package com.cskaoyan.mall;
+
+/**
+ * @author xujiangzhe <xujiangzhe@kuaishou.com>
+ * Created on 2020-12-10
+ */
+public class Dev {
+    public static void main(String[] args) {
+        System.out.println("Devvvvv");
+    }
+}
