d7989ebba03e01d9bdb3b7953f288988115bc3c1
xujiangzhe <xujiangzhe@kuaishou.com>
2020-12-10 14:22:54 +0800

    123

---
+++ src/main/java/com/cskaoyan/mall/Test.java
@@ -0,0 +1,11 @@
+package com.cskaoyan.mall;
+
+/**
+ * @author xujiangzhe <xujiangzhe@kuaishou.com>
+ * Created on 2020-12-10
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("fuckkkk");
+    }
+}
